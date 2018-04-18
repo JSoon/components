6f634f3206246c7211851df1ac48a711bfdeb0d3
Soon <serdeemail@gmail.com>
2018-04-18 15:23:15 +0800

    1. 优化组件views目录结构 2. 新增日期时间选择器组件

---
+++ traditional/public/components/datetimepicker/datetimepicker.js
@@ -0,0 +1,31 @@
+/**
+ * 日期时间选择器
+ * @requires laydate v5.0.9
+ * @link http://www.layui.com/laydate/
+ * 
+ * @author J.Soon <serdeemail@gmail.com>
+ */
+(function (root, factory) {
+    // 组件名称
+    var comPrefix = 'com_'; // 前缀
+    var comName = 'datetimepicker'; // 名称
+    var globalName = comPrefix + comName; // 全局名称
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        // Defines a module "amdWebGlobal" that depends another module called "b".
+        define(['layui-laydate'], function (laydate) {
+            // Also create a global in case some scripts
+            // that are loaded still are looking for
+            // a global even when an AMD loader is in use.
+            return (root[globalName] = factory(laydate));
+        });
+    } else {
+        // Browser globals
+        root[globalName] = factory(root.laydate);
+    }
+}(typeof self !== 'undefined' ? self : this, function (laydate) {
+
+    return laydate.render;
+
+}));
