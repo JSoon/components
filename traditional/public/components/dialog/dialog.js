406eb3c6466fbabc74fb4f733444e0e8f59ab051
Soon <serdeemail@gmail.com>
2018-05-02 17:55:55 +0800

    【dialog】 1. 优化组件方法，完善组件回调方法——onCreate、onDestroy、onClose、onShow 2. 新增loading对话框

--- traditional/public/components/dialog/dialog.js
+++ traditional/public/components/dialog/dialog.js
@@ -27,6 +27,9 @@
     }
 }(typeof self !== 'undefined' ? self : this, function (b) {
 
+    // 常量定义
+    var ANIMATE_HIDE = 'removing'; // 隐藏动画classname
+
     /**
      * 弹出式对话框
      */
@@ -47,11 +50,14 @@
          * @param   {boolean}   opts.overlay    点击overlay区域是否关闭对话框，默认true
          * 公共事件
          * @param   {function}  opts.onCreate   创建对话框回调函数
-         * @param   {function}  opts.onRemove   移除对话框回调函数
+         * @param   {function}  opts.onRemove   移除对话框回调函数（当且仅当对话框存在时触发，若当前对话框已经被移除，则该回调无效）
+         * @param   {function}  opts.onShow     显示对话框回调函数（当且仅当对话框存在时触发，若当前对话框已经被移除，则该回调无效）
+         * @param   {function}  opts.onClose    隐藏对话框回调函数（当且仅当对话框存在时触发，若当前对话框已经被移除，则该回调无效）
          * 确认对话框事件
          * @param   {function}  opts.onConfirm  确认按钮回调函数
          */
         function dialog(opts) {
+            var dialog = {}; // 对外暴露接口
             opts = opts || {};
             opts.width = opts.width || 400;
             opts.overlay = opts.overlay === false ? false : true;
@@ -62,6 +68,7 @@
             var $html = $(opts.template(opts));
             // 添加到页面
             $('body').append($html);
+            dialog.html = $html;
 
             // 对话框各部分元素
             var $dialog = $html.find('.dialog');
@@ -92,31 +99,56 @@
             //#endregion
 
             // 创建对话框回调函数
-            typeof opts.onCreate === 'function' && opts.onCreate($dialog);
+            typeof opts.onCreate === 'function' && opts.onCreate($html);
 
             // 点击关闭按钮或者对话框之外的overlay遮罩部分，销毁对话框
             $html.on('click', function (e) {
                 if (e.target.getAttribute('class').indexOf('J_DialogClose') !== -1) {
-                    closeDialog($html);
+                    destroy();
                 }
                 if (opts.overlay && e.currentTarget === e.target) {
-                    closeDialog($html);
+                    destroy();
                 }
             });
 
-            /**
-             * 关闭对话框，并移除DOM节点
-             * @param {object} overlay 对话框父容器jquery对象
-             */
-            function closeDialog(overlay) {
-                // 移除对话框回调函数
-                typeof opts.onRemove === 'function' && opts.onRemove($dialog);
-                var duration = 200; // 移除对话框时间，与移除动画持续时间相同
-                overlay.addClass('removing'); // 添加移除中动画
-                setTimeout(function () {
-                    overlay.remove();
-                }, duration);
-                return overlay;
+            // 销毁对话框
+            dialog.destroy = destroy;
+
+            function destroy() {
+                if (!$html.length) {
+                    return dialog;
+                }
+                // 销毁对话框回调函数
+                typeof opts.onRemove === 'function' && opts.onRemove($html);
+                destroyDialog($html, true);
+                dialog.html = null;
+                return dialog;
+            }
+
+            // 隐藏对话框
+            dialog.close = close;
+
+            function close() {
+                if (!$html.length) {
+                    return dialog;
+                }
+                // 隐藏对话框回调函数
+                typeof opts.onClose === 'function' && opts.onClose($html);
+                destroyDialog($html, false);
+                return dialog;
+            }
+
+            // 显示对话框
+            dialog.show = show;
+
+            function show() {
+                if (!$html.length) {
+                    return dialog;
+                }
+                // 显示对话框回调函数
+                typeof opts.onShow === 'function' && opts.onShow($html);
+                $html.removeClass(ANIMATE_HIDE);
+                return dialog;
             }
 
             /**
@@ -135,7 +167,7 @@
                 return dialog;
             }
 
-            return $dialog;
+            return dialog;
         }
 
         that.normal = normal;
@@ -148,11 +180,15 @@
             // dialogTemplate为预编译后的模板函数，若没有，则采用AMD的方式进行装载（需要使用AMD加载器或者使用webpack）
             var tmpl = typeof window.dialogTemplate !== 'undefined' ? window.dialogTemplate : require('./dialog.pug');
             opts.template = tmpl;
+            var normalDialog;
+
             try {
-                var $normalDialog = dialog(opts);
+                normalDialog = dialog(opts);
             } catch (err) {
                 console.error(err);
             }
+
+            return normalDialog;
         }
 
         that.confirm = confirm;
@@ -165,44 +201,125 @@
             // confirmTemplate为预编译后的模板函数，若没有，则采用AMD的方式进行装载（需要使用AMD加载器或者使用webpack）
             var tmpl = typeof window.confirmTemplate !== 'undefined' ? window.confirmTemplate : require('./confirm.pug');
             opts.template = tmpl;
+            var confirmDialog;
+
             try {
-                var $confirmDialog = dialog(opts);
+                confirmDialog = dialog(opts);
             } catch (err) {
                 console.error(err);
             }
             // 确认按钮
-            $confirmDialog.find('.J_DialogConfirm').on('click', function () {
-                typeof opts.onConfirm === 'function' && opts.onConfirm($confirmDialog);
+            confirmDialog.html.find('.J_DialogConfirm').on('click', function () {
+                typeof opts.onConfirm === 'function' && opts.onConfirm(confirmDialog.html);
             });
+
+            return confirmDialog;
         }
 
         that.toast = toast;
+        that.loading = toast;
         /**
          * 简单信息提示
          * @description 没有任何交互性的提示性对话框
-         * @param {string}  content    提示内容
-         * @param {number}  duration   内容显示时间
+         * @param   {object}    opts
+         * @param   {string}    opts.content    提示内容
+         * @param   {number}    opts.duration   内容显示时间
+         * @param   {function}  opts.onCreate   创建对话框回调函数
+         * @param   {function}  opts.onRemove   移除对话框回调函数（当且仅当对话框存在时触发，若当前对话框已经被移除，则该回调无效）
+         * @param   {function}  opts.onShow     显示对话框回调函数（当且仅当对话框存在时触发，若当前对话框已经被移除，则该回调无效）
+         * @param   {function}  opts.onClose    隐藏对话框回调函数（当且仅当对话框存在时触发，若当前对话框已经被移除，则该回调无效）
          */
-        function toast(content, duration) {
+        function toast(opts) {
+            var toast = {}; // 对外暴露接口
             // 加载前端模板
             // toastTemplate为预编译后的模板函数，若没有，则采用AMD的方式进行装载（需要使用AMD加载器或者使用webpack）
             var tmpl = typeof window.toastTemplate !== 'undefined' ? window.toastTemplate : require('./toast.pug');
             // 编译为html字符串，并转换成jquery对象
             var $html = $(tmpl({
-                content: content
+                content: opts.content
             }));
             // 添加到页面
             $('body').append($html);
+            toast.html = $html;
+
+            // 创建对话框回调函数
+            typeof opts.onCreate === 'function' && opts.onCreate($html);
 
             // 设置内容消失时间
-            duration = duration || 2000;
-            setTimeout(function () {
-                var duration = 200; // 移除对话框时间，与移除动画持续时间相同
-                $html.addClass('removing'); // 添加移除中动画
+            var duration = opts.duration || 2000;
+
+            // 若toast持续时间不为-1，则在duration毫秒后消失
+            if (duration !== -1) {
                 setTimeout(function () {
-                    $html.remove();
+                    var duration = 200; // 移除对话框时间，与移除动画持续时间相同
+                    $html.addClass(ANIMATE_HIDE); // 添加移除中动画
+                    setTimeout(function () {
+                        $html.remove();
+                    }, duration);
                 }, duration);
-            }, duration);
+            }
+            // 若为-1，则永远不消失，直到手动调用destroyDialog方法
+            else {
+                // ...
+            }
+
+            // 销毁对话框
+            toast.destroy = destroy;
+
+            function destroy() {
+                if (!$html.length) {
+                    return toast;
+                }
+                // 销毁对话框回调函数
+                typeof opts.onRemove === 'function' && opts.onRemove($html);
+                destroyDialog($html, true);
+                toast.html = null;
+                return toast;
+            }
+
+            // 隐藏对话框
+            toast.close = close;
+
+            function close() {
+                if (!$html.length) {
+                    return toast;
+                }
+                // 隐藏对话框回调函数
+                typeof opts.onClose === 'function' && opts.onClose($html);
+                destroyDialog($html, false);
+                return toast;
+            }
+
+            // 显示对话框
+            toast.show = show;
+
+            function show() {
+                if (!$html.length) {
+                    return toast;
+                }
+                // 显示对话框回调函数
+                typeof opts.onShow === 'function' && opts.onShow($html);
+                $html.removeClass(ANIMATE_HIDE);
+                return toast;
+            }
+
+            return toast;
+        }
+
+        /**
+         * 关闭对话框，并移除DOM节点
+         * @param {object}  html    整个对话框节点jquery对象
+         * @param {boolean} remove  是否移对话框节点，true：移除，false：仅隐藏
+         */
+        function destroyDialog(html, remove) {
+            var duration = 200; // 移除对话框时间，与移除动画持续时间相同
+            html.addClass(ANIMATE_HIDE); // 添加移除中动画
+            if (remove) {
+                setTimeout(function () {
+                    html.remove();
+                }, duration);
+            }
+            return html;
         }
 
         return that;
